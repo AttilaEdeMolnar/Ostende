86a337385852777c0dcd1726b1e19979dc2d98cc
Molnár Attila Ede <chaserxrd@gmail.com>
2023-02-06 14:03:29 +0100

    Login panel

---
+++ include/login.php
@@ -0,0 +1,57 @@
+<div class="container">
+  <div class="row">
+
+
+    <div class="col">
+    <h1>Bejelentkezés</h1>
+  <p class="BiggerBr"></p>
+<form method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Maradj bejelentkezve</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+</form>
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+    <h1>Regisztráció</h1>
+  <p class="BiggerBr"></p>
+<form method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">ÁSZF</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Regisztráció</button>
+</form>
+</div>
+</div>
+</div>
